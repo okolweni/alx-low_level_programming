18b1c34333a1e277720d36577548979129f07cd5
okolweni <okolweni@gmail.com>
2023-02-17 06:12:51 -0800

    fixing another error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /**
- *main -  This program will assign a random number to the variable n each time it is executed.
+ *main -  Assign a random number to the variable n each time it is executed.
  * if the number is greater than 0: is positive
  * if the number is 0: is zero
  * if the number is less than 0: is negative
