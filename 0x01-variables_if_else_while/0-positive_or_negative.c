967cdd3ddff627cef175c6496e3813b8562fa340
okolweni <okolweni@gmail.com>
2023-02-17 05:50:14 -0800

    My first c code, if command

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *main -  This program will assign a random number to the variable n each time it is executed.
+ * if the number is greater than 0: is positive
+ * if the number is 0: is zero
+ * if the number is less than 0: is negative
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n == 0)
+{
+		printf("%d is zero\n", n);
+}	
+	else if (n > 0)
+{	
+		printf("%d is positive\n", n);
+}
+	else if (n < 0)
+{
+		printf("%d is negative\n", n);
+}
+	return (0);
+
+}
