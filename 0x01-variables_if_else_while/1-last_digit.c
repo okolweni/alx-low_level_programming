1d9687dd1d4f2564e8e45ae7c359189d3b9b4eb2
okolweni <okolweni@gmail.com>
2023-02-17 07:25:18 -0800

    Adding a file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Assign arandom number to the variable n each time it is executed.
+ * if the last digit of n > 5 : and is greater than 5
+ * if the last digit of n = 0 : and is 0
+ * if the last digit of n < 5 : and is less than 6 and not 0
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	int n, Y;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	Y = n % 10
+
+	if (Y > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, Y);
+	}
+	if else(Y == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, Y);
+	}
+	if else(Y < 5)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Y);
+	}
+	return (0);
+
+}
