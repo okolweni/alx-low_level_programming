62abbc9a56117692cdbbb1d7ea539aca2c608cdd
okolweni <okolweni@gmail.com>
2023-02-18 01:51:32 -0800

    fixing my chars

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,17 +9,17 @@
 
 int main(void)
 {
-	int intType;
-	float floatType;
+	char charType;
+	int  intType;
 	long int longintType;
 	long long int longlongintType;
-	char charType;
+	float  floatType;
 
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
-	printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
