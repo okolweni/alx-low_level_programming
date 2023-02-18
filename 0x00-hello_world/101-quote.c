d5be19425017b1476ba5db81695b6037539f374f
okolweni <okolweni@gmail.com>
2023-02-18 02:42:34 -0800

    testing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * discription - prints the exact sentence,
+ * followed by a new line, to the standard error
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19");
+
+	return (1);
+
+}
+
