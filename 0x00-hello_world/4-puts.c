8da5a1bf5b32d43808441e2e83207560239989ea
okolweni <okolweni@gmail.com>
2023-02-18 00:52:39 -0800

    Fixing my code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - print the exact sentence, follwed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	puts("Programming is like building a multilingual puzzle)
+	puts("Programming is like building a multilingual puzzle");
 		return (0);
 }
