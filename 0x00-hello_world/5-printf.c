40679ea63310cd6afcb9a3a45074d463537bbfb5
okolweni <okolweni@gmail.com>
2023-02-18 01:21:25 -0800

    Program that prints the exact sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Discription : Program that prints an exact senstence,
+ * followed by a line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
