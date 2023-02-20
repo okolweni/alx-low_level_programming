b1f9627599cefe4764ee440ad570ffe4736f3001
okolweni <okolweni@gmail.com>
2023-02-20 11:25:28 -0800

    different number combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription - prints all possible different combinations of 2 digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x < '9' ; x++)
+	{
+		for (y = x + 1; y <= '9' ; y++)
+		{
+			if (y != x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x == '8' && y == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
