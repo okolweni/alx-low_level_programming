b4f1596acfaade5c9d86ebb79e6cb3c6b201d981
okolweni <okolweni@gmail.com>
2023-02-21 02:16:51 -0800

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription - prints _putchar , follwed bya new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x = '_putchar';
+
+	putchar(x);
+
+	return (0);
+}
