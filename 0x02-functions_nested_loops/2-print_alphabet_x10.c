73458b82193aee512e7aa250a47b2448f3b28daa
okolweni <okolweni@gmail.com>
2023-02-22 06:48:28 -0800

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int x = 0;
 
-	while (x <= '9')
+	while (x <= 9)
 	{
 		for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 		{
 			_putchar(alphabet);
 		}
-	}
 	_putchar('\n');
 	x++;
+	}
 }
