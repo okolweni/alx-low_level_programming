18e6a94f8b369d7e7fc147ffabf73c89ebce071c
okolweni <okolweni@gmail.com>
2023-02-27 11:12:30 -0800

    fixing my code

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * _strlen - function that returns the length of a string
  *
  * @str: string length to be calculated
  *
- * Return: length of @str
+ * Return: length of @str.
  */
 
 size_t _strlen(const char *str)
