2e5cd92bb9056e9a0e4d74504949ed52f33fb32f
okolweni <okolweni@gmail.com>
2023-05-18 07:12:13 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Description: program that prints an exact sentence, followed by a new line.
- * 		use the function puts instead of printf.
+ * use the function puts instead of printf.
  *
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
