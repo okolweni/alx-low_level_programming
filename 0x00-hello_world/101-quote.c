32f8911558867db8da10c139ec962fe39fe7cd51
okolweni <okolweni@gmail.com>
2023-05-18 11:03:36 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prom that prints specific code, follwed a new line,
+ * to the standard error.
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "\n");
+
+return (1);
+}
