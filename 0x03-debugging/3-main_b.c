f482ea82c388c4d09ee01cec11e80bafafe66da3
okolweni <okolweni@gmail.com>
2023-05-25 07:51:29 -0700

    fixing 3. Leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -4,6 +4,9 @@
 /**
 * main - takes a date and prints how many days are left in the year, taking
 * leap years into account
+* @month: month in number format
+* @day: days of the month
+*
 * Return: 0
 */
 
@@ -15,11 +18,11 @@ int main(void)
 	int month;
 	int day;
 	int year;
-	
+
 	month = 02;
 	day = 29;
 	year = 2000;
-	
+
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 	day = convert_day(month, day);
