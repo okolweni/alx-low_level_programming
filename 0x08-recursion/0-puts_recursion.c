f0ac5d063e85665e488416dce0663ad19eef8218
okolweni <okolweni@gmail.com>
2023-06-07 05:38:34 -0700

    Write a function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s: pointer
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
