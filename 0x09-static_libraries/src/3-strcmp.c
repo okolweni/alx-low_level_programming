132b9c249dd22441c95d84cc2f6592e3830d24cf
okolweni <okolweni@gmail.com>
2023-06-15 13:51:51 -0700

    Create the static library

---
+++ 0x09-static_libraries/src/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: less than 0, if s1 < s2,
+ * greater than 0, if s1 > s2,
+ * equal to 0, if s1 == s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
