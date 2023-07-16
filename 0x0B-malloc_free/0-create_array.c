4e96ded7eaca8f797a98c4b5c1f2a30ac8002d6f
okolweni <okolweni@gmail.com>
2023-07-16 02:50:17 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *array = (char *)malloc((size + 1) * sizeof(char));
+	char *array = (char *)malloc((size) * sizeof(char));
 	unsigned int i;
 
 	if (size == 0)
