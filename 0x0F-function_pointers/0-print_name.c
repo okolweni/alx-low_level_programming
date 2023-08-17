d1d9f77162a6911d75559f29862c17afa6e95d38
okolweni <okolweni@gmail.com>
2023-08-17 03:09:55 -0700

    Completed the 1st half of the project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: string to be printed.
+ * @f: pointer to the string.
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
